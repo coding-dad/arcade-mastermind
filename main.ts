408fb646d3c542e34a2399896b687a1bacde5679
coding-dad <52372171+coding-dad@users.noreply.github.com>
2020-04-12 20:53:24 +0200

    working navigation and color selection

--- main.ts
+++ main.ts
@@ -3,28 +3,13 @@ namespace SpriteKind {
     export const PinZeiger = SpriteKind.create()
 }
 function setupReihe (num: number) {
+    _spriteReiheWerte = [0, 0, 0, 0]
     tempY = scene.screenHeight() - 10 - num * 20
     for (let Index2 = 0; Index2 <= 3; Index2++) {
         tempX = 10 + Index2 * 20
-        mySprite = sprites.create(img`
-b b b b b b b b b b b b b b b b 
-b . . . . . . . . . . . . . . b 
-b . . . . f f f f f f . . . . b 
-b . . . f . . . . . . f . . . b 
-b . . f . . . . . . . . f . . b 
-b . f . . . . . . . . . . f . b 
-b . f . . . . . . . . . . f . b 
-b . f . . . . . . . . . . f . b 
-b . f . . . . . . . . . . f . b 
-b . f . . . . . . . . . . f . b 
-b . f . . . . . . . . . . f . b 
-b . . f . . . . . . . . f . . b 
-b . . . f . . . . . . f . . . b 
-b . . . . f f f f f f . . . . b 
-b . . . . . . . . . . . . . . b 
-b b b b b b b b b b b b b b b b 
-`, SpriteKind.Pin)
-        mySprite.setPosition(tempX, tempY)
+        tempSprite = sprites.create(_FarbPins[0], SpriteKind.Pin)
+        tempSprite.setPosition(tempX, tempY)
+        _spriteReiheSprites.push(tempSprite)
     }
 }
 function setzePosZeiger () {
@@ -36,6 +21,22 @@ controller.anyButton.onEvent(ControllerButtonEvent.Pressed, function () {
     if (controller.left.isPressed() && _aktuelleX > 0) {
         _aktuelleX += -1
         setzePosZeiger()
+    } else if (controller.up.isPressed()) {
+        tempVal = _spriteReiheWerte[_aktuelleX]
+        tempVal += 1
+        if (tempVal >= _FarbPins.length) {
+            tempVal = 1
+        }
+        _spriteReiheWerte[_aktuelleX] = tempVal
+        setzeFarbe(_aktuelleX, tempVal)
+    } else if (controller.down.isPressed()) {
+        tempVal = _spriteReiheWerte[_aktuelleX]
+        tempVal -= 1
+        if (tempVal < 1) {
+            tempVal = 1
+        }
+        _spriteReiheWerte[_aktuelleX] = tempVal
+        setzeFarbe(_aktuelleX, tempVal)
     } else {
         if (controller.right.isPressed() && _aktuelleX < 3) {
             _aktuelleX += 1
@@ -43,37 +44,129 @@ controller.anyButton.onEvent(ControllerButtonEvent.Pressed, function () {
         }
     }
 })
+function setzeFarbe (posX: number, value: number) {
+    tempSprite = _spriteReiheSprites[posX]
+    tempSprite.setImage(_FarbPins[value]);
+}
 function setupSpiel () {
     scene.setBackgroundColor(1)
+    _FarbPins = [img`
+        b b b b b b b b b b b b b b b b
+        b . . . . . . . . . . . . . . b
+        b . . . . f f f f f f . . . . b
+        b . . . f d d d d d d f . . . b
+        b . . f d d d d d d d d f . . b
+        b . f d d d d d d d d d d f . b
+        b . f d d d d d d d d d d f . b
+        b . f d d d d d d d d d d f . b
+        b . f d d d d d d d d d d f . b
+        b . f d d d d d d d d d d f . b
+        b . f d d d d d d d d d d f . b
+        b . . f d d d d d d d d f . . b
+        b . . . f d d d d d d f . . . b
+        b . . . . f f f f f f . . . . b
+        b . . . . . . . . . . . . . . b
+        b b b b b b b b b b b b b b b b
+   `, img`
+        b b b b b b b b b b b b b b b b
+        b . . . . . . . . . . . . . . b
+        b . . . . f f f f f f . . . . b
+        b . . . f 2 2 2 2 2 2 f . . . b
+        b . . f 2 2 2 2 2 2 2 2 f . . b
+        b . f 2 2 2 2 2 2 2 2 2 2 f . b
+        b . f 2 2 2 2 2 2 2 2 2 2 f . b
+        b . f 2 2 2 2 2 2 2 2 2 2 f . b
+        b . f 2 2 2 2 2 2 2 2 2 2 f . b
+        b . f 2 2 2 2 2 2 2 2 2 2 f . b
+        b . f 2 2 2 2 2 2 2 2 2 2 f . b
+        b . . f 2 2 2 2 2 2 2 2 f . . b
+        b . . . f 2 2 2 2 2 2 f . . . b
+        b . . . . f f f f f f . . . . b
+        b . . . . . . . . . . . . . . b
+        b b b b b b b b b b b b b b b b
+   `, img`
+        b b b b b b b b b b b b b b b b
+        b . . . . . . . . . . . . . . b
+        b . . . . f f f f f f . . . . b
+        b . . . f 5 5 5 5 5 5 f . . . b
+        b . . f 5 5 5 5 5 5 5 5 f . . b
+        b . f 5 5 5 5 5 5 5 5 5 5 f . b
+        b . f 5 5 5 5 5 5 5 5 5 5 f . b
+        b . f 5 5 5 5 5 5 5 5 5 5 f . b
+        b . f 5 5 5 5 5 5 5 5 5 5 f . b
+        b . f 5 5 5 5 5 5 5 5 5 5 f . b
+        b . f 5 5 5 5 5 5 5 5 5 5 f . b
+        b . . f 5 5 5 5 5 5 5 5 f . . b
+        b . . . f 5 5 5 5 5 5 f . . . b
+        b . . . . f f f f f f . . . . b
+        b . . . . . . . . . . . . . . b
+        b b b b b b b b b b b b b b b b
+   `, img`
+        b b b b b b b b b b b b b b b b
+        b . . . . . . . . . . . . . . b
+        b . . . . f f f f f f . . . . b
+        b . . . f 7 7 7 7 7 7 f . . . b
+        b . . f 7 7 7 7 7 7 7 7 f . . b
+        b . f 7 7 7 7 7 7 7 7 7 7 f . b
+        b . f 7 7 7 7 7 7 7 7 7 7 f . b
+        b . f 7 7 7 7 7 7 7 7 7 7 f . b
+        b . f 7 7 7 7 7 7 7 7 7 7 f . b
+        b . f 7 7 7 7 7 7 7 7 7 7 f . b
+        b . f 7 7 7 7 7 7 7 7 7 7 f . b
+        b . . f 7 7 7 7 7 7 7 7 f . . b
+        b . . . f 7 7 7 7 7 7 f . . . b
+        b . . . . f f f f f f . . . . b
+        b . . . . . . . . . . . . . . b
+        b b b b b b b b b b b b b b b b
+   `, img`
+        b b b b b b b b b b b b b b b b
+        b . . . . . . . . . . . . . . b
+        b . . . . f f f f f f . . . . b
+        b . . . f 6 6 6 6 6 6 f . . . b
+        b . . f 6 6 6 6 6 6 6 6 f . . b
+        b . f 6 6 6 6 6 6 6 6 6 6 f . b
+        b . f 6 6 6 6 6 6 6 6 6 6 f . b
+        b . f 6 6 6 6 6 6 6 6 6 6 f . b
+        b . f 6 6 6 6 6 6 6 6 6 6 f . b
+        b . f 6 6 6 6 6 6 6 6 6 6 f . b
+        b . f 6 6 6 6 6 6 6 6 6 6 f . b
+        b . . f 6 6 6 6 6 6 6 6 f . . b
+        b . . . f 6 6 6 6 6 6 f . . . b
+        b . . . . f f f f f f . . . . b
+        b . . . . . . . . . . . . . . b
+        b b b b b b b b b b b b b b b b
+    `]
     _aktuelleX = 0
     _aktuelleY = 0
-    for (let Index = 0; Index <= 4; Index++) {
-        setupReihe(Index)
-    }
+    setupReihe(_aktuelleY)
     _posZeiger = sprites.create(img`
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-2 . . . . . . . . . . . . . . 2 
-2 . . . . . . . . . . . . . . 2 
-2 . . . . . . . . . . . . . . 2 
-2 . . . . . . . . . . . . . . 2 
-2 . . . . . . . . . . . . . . 2 
-2 . . . . . . . . . . . . . . 2 
-2 . . . . . . . . . . . . . . 2 
-2 . . . . . . . . . . . . . . 2 
-2 . . . . . . . . . . . . . . 2 
-2 . . . . . . . . . . . . . . 2 
-2 . . . . . . . . . . . . . . 2 
-2 . . . . . . . . . . . . . . 2 
-2 . . . . . . . . . . . . . . 2 
-2 . . . . . . . . . . . . . . 2 
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-`, SpriteKind.PinZeiger)
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+        2 . . . . . . . . . . . . . . 2
+        2 . . . . . . . . . . . . . . 2
+        2 . . . . . . . . . . . . . . 2
+        2 . . . . . . . . . . . . . . 2
+        2 . . . . . . . . . . . . . . 2
+        2 . . . . . . . . . . . . . . 2
+        2 . . . . . . . . . . . . . . 2
+        2 . . . . . . . . . . . . . . 2
+        2 . . . . . . . . . . . . . . 2
+        2 . . . . . . . . . . . . . . 2
+        2 . . . . . . . . . . . . . . 2
+        2 . . . . . . . . . . . . . . 2
+        2 . . . . . . . . . . . . . . 2
+        2 . . . . . . . . . . . . . . 2
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+    `, SpriteKind.PinZeiger)
     setzePosZeiger()
 }
+let _spriteReiheSprites: Sprite[] = []
+let _spriteReiheWerte: number[] = []
 let _posZeiger: Sprite = null
 let _aktuelleY = 0
 let _aktuelleX = 0
-let mySprite: Sprite = null
+let _FarbPins: Image[] = []
+let tempSprite: Sprite = null
 let tempX = 0
 let tempY = 0
+let tempVal = 0
 setupSpiel()
